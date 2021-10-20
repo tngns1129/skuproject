f3084ff74c342e0c1915c3f5fdb16b9c8c5a4018
HoGil2 <rlf0303@gmail.com>
2021-10-20 21:14:24 +0900

    ready for modify architecture

---
+++ app/src/main/java/com/familyset/myapplication/ui/record/RecordFragment.java
@@ -0,0 +1,27 @@
+package com.familyset.myapplication.ui.record;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.familyset.myapplication.R;
+
+public class RecordFragment extends Fragment {
+
+    private RecordFragmentViewModel recordFragmentViewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_record, container, false);
+        recordFragmentViewModel = new RecordFragmentViewModel(view);
+        return view;
+
+    }
+
+}
